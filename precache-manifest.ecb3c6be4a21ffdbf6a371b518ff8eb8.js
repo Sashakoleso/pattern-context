342ec0f5c117d21d608511d69376bc611a792b33
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-03 09:25:24 +0200

    Updates

---
+++ precache-manifest.ecb3c6be4a21ffdbf6a371b518ff8eb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e0b112a715ca88490ed8e54ff82d17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8589fd5cf387f628e2f9",
+    "url": "/static/css/main.e38f4f2e.chunk.css"
+  },
+  {
+    "revision": "ec670dd03932739141ef",
+    "url": "/static/js/2.fa6d69d8.chunk.js"
+  },
+  {
+    "revision": "8589fd5cf387f628e2f9",
+    "url": "/static/js/main.f60c176c.chunk.js"
+  },
+  {
+    "revision": "63bcc2513170116514b0",
+    "url": "/static/js/runtime-main.d67d228f.js"
+  }
+]);
